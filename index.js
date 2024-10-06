7fc38618d83d76a0c5c0b961e738825d894d113d
+ <+>
2024-10-06 20:17:12 +0300

    destructuring assignment

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = 'cow horse sheep pig chicken'
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ')
+console.log(moo, neigh, baa, oink, cluck)
+const [bessie, , dolly, babe, little] = farmAnimals.split(' ')
+console.log(bessie, dolly, babe, little)
+const [blackAndWhite, , black, pink] = farmAnimals.split(' ')
+console.log(blackAndWhite, black, pink)
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+const [red, orange, yellow, green, blue, indigo, violet] = colors
+console.log(red, orange, yellow, green, blue, indigo, violet)
+const [r, o, y, g, b, , v] = colors
+console.log(r, o, y, g, b, v)
+const [ , , , , , indg] = colors
+console.log(indg)
+
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -8,7 +24,9 @@ const muppet = {
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
-};
+}
+const {muppetName, color, song, job, partner} = muppet
+console.log(muppetName, color, song, job, partner)
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -23,7 +41,9 @@ const nestedMuppet = {
   },
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
-};
+}
+const { album: { theMuppetMovie: {song2, song4 }}, nestedJob, nestedPartner} = nestedMuppet
+console.log(song2, song4, nestedJob, nestedPartner)
 
 // Strings
 
